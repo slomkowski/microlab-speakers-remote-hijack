59ff6f49a6940922e7602d1af33637c8c1f99a35
Michał Słomkowski <m.slomkowski@gmail.com>
2020-10-22 17:25:43 +0200

    Implementation.

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 Compiled under *avr-gcc*, *CMake* required. *burn.sh* - flashing.
 
 TODO website
+
+## Programming
+
+compile and run:
+
+```
+ micronucleus --run build/microlab-speaker-remote-hijack.hex
+```
+
+Then plug the Digispark module. It is programmed.
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,70 @@
 #include <avr/io.h>
-#include <avr/wdt.h>
 #include <util/delay.h>
-#include <avr/interrupt.h>
-#include <string.h>
-#include <stdio.h>
+#include <avr/sleep.h>
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wmissing-noreturn"
+constexpr uint16_t ADDRESS = 0xfe01;
+constexpr uint16_t COMMAND_POWER = 0xfc03;
+constexpr uint16_t COMMAND_SWITCH_INPUT = 0xfb04;
 
-int main() {
-#define DELAY_MS    500
 
-    DDRB &= ~_BV(0);
-    PORTB &= ~_BV(0);
-
-    for (;;) {
-        _delay_ms(DELAY_MS);
-    }
+static inline void carrierTurnOn() {
+    DDRB |= _BV(0);
 }
 
-#pragma clang diagnostic pop
+static inline void carrierTurnOff() {
+    DDRB &= ~_BV(0);
+}
+
+static void sendWord(uint16_t data) {
+    uint16_t mask = 1;
+    do {
+        carrierTurnOn();
+        _delay_ms(0.5625);
+        carrierTurnOff();
+
+        if (data & mask) {
+            _delay_ms(1.6875);
+        } else {
+            _delay_ms(0.5625);
+        }
+        mask = mask << 1u;
+    } while (mask);
+}
+
+static void sendNecCode(uint16_t address, uint16_t command) {
+    PORTB |= _BV(1);
+
+    carrierTurnOn();
+    _delay_ms(9.0);
+    carrierTurnOff();
+    _delay_ms(4.5);
+
+    sendWord(address);
+    sendWord(command);
+
+    carrierTurnOn();
+    _delay_ms(0.5625);
+    carrierTurnOff();
+
+    PORTB &= ~_BV(1);
+}
+
+int main() {
+    DDRB |= _BV(1);
+    PORTB &= ~_BV(0);
+
+    carrierTurnOff();
+
+    _delay_ms(10.0);
+
+    sendNecCode(ADDRESS, COMMAND_POWER);
+
+    _delay_ms(500.0);
+    _delay_ms(500.0);
+    _delay_ms(500.0);
+
+    sendNecCode(ADDRESS, COMMAND_SWITCH_INPUT);
+
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_enable();
+}
